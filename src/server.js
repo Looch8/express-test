e388fa41b3737f5256e13cf4196b25179afb0507
Luke Wheldale <lukewheldale@gmail.com>
2023-10-21 10:31:07 +1030

    feat: request post implemented

--- src/server.js
+++ src/server.js
@@ -6,9 +6,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// This lets us read POST'd JSON body data on the reuqest
+app.use(express.json());
+
+// Configuratuion goes above ^^^^
+// Routes go below
 app.get("/", (request, response) => {
 	response.json({
 		message: "Hello World",
+	});
+});
+
+app.post("/", (request, response) => {
+	response.json({
+		received: request.body.message,
 	});
 });
 
